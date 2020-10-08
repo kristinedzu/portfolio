a371b8015090d9fa32394bb53f9a224e036181c7
Kristine Dzumakajeva <kristinedzu@gmail.com>
2020-10-08 18:48:44 +0200

    responsive main

--- js/js.js
+++ js/js.js
@@ -58,41 +58,38 @@ function goBack() {
 
 /* development animation */
 
-var scroll = window.requestAnimationFrame ||
+let scroll = window.requestAnimationFrame ||
 
-    function (callback) {
-        window.setTimeout(callback, 1000 / 60)
-    };
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
+    function (callback) { window.setTimeout(callback, 1000 / 60) };
+let elementsToShow = document.querySelectorAll('.show-on-scroll');
 
 function loop() {
-    elementsToShow.forEach(function (element) {
+
+    Array.prototype.forEach.call(elementsToShow, function (element) {
         if (isElementInViewport(element)) {
             element.classList.add('is-visible');
         } else {
             element.classList.remove('is-visible');
         }
     });
+
     scroll(loop);
 }
 
 loop();
 
-// Call the loop for the first time
-
-
 function isElementInViewport(el) {
-    console.log('is element in viewport')
-
     if (typeof jQuery === "function" && el instanceof jQuery) {
         el = el[0];
     }
-    var rect = el.getBoundingClientRect();
+    let rect = el.getBoundingClientRect();
     return (
-        (rect.top <= 0 &&
-            rect.bottom >= 0) ||
+        (rect.top <= 0
+            && rect.bottom >= 0)
+        ||
         (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
         (rect.top >= 0 &&
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     );
