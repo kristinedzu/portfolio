44cfbb86be3db16a213ff91fe4add78c30aa80ef
Kristine Dzumakajeva <kristinedzu@gmail.com>
2020-10-19 13:56:56 +0200

    about me page is responsive

--- js/js.js
+++ js/js.js
@@ -94,3 +94,22 @@ function isElementInViewport(el) {
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     );
 }
+
+
+/* read more button */
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMore");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
